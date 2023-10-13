44d91913118168cbac7d275bb930c508a6e751a5
Feng Lu <33820938+Lu-Feng@users.noreply.github.com>
2023-10-13 13:44:34 +0800

    Update util.py

--- util.py
+++ util.py
@@ -34,13 +34,13 @@ def resume_train(args, model, optimizer=None, strict=False):
     model.load_state_dict(checkpoint["model_state_dict"], strict=strict)
     if optimizer:
         optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-    best_r5 = checkpoint["best_r5"]
+    best_r1pr5 = checkpoint["best_r1pr5"]
     not_improved_num = checkpoint["not_improved_num"]
     logging.debug(f"Loaded checkpoint: start_epoch_num = {start_epoch_num}, " \
-                  f"current_best_R@5 = {best_r5:.1f}")
+                  f"current_best_R@1+R@5 = {best_r1pr5:.1f}")
     if args.resume.endswith("last_model.pth"):  # Copy best model to current save_dir
         shutil.copy(args.resume.replace("last_model.pth", "best_model.pth"), args.save_dir)
-    return model, optimizer, best_r5, start_epoch_num, not_improved_num
+    return model, optimizer, best_r1pr5, start_epoch_num, not_improved_num
 
 
 def compute_pca(args, model, pca_dataset_folder, full_features_dim):
